37fca706e4b5351a5f1a29ebcf37b92a16e255dc
Seamus Naaykens <snaaykens@gmail.com>
2023-01-14 15:40:14 -0700

    created initial style and structure for the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,18 @@
 import './Header.scss'
 import { Link } from 'react-router-dom'
+import logo from '../../assets/icons/sn-logo.jpg'
+import menu from '../../assets/icons/menu-button.png'
 
 function Header() {
     return (
         <div className='header'>
             <div className='header__container--1'>
                 <Link className='header__logo-link'>
-                    <img alt='Seamus Naaykens Logo'/>
+                    <img className='header__logo' src={logo} alt='Seamus Naaykens Logo' />
                 </Link>
             </div>
-            <div className='header__container--2'>
+            <div className='header__container--2'> 
+            <img className='header__menu-button' src={menu} alt='menu button'/>
                 <Link className='header__link'>HOME</Link>
                 <Link className='header__link'>ABOUT</Link>
                 <Link className='header__link'>PROJECTS</Link>
