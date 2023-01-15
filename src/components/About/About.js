1e9d8afaf6ad9d58950098186fd5413a34ac41a5
Seamus Naaykens <snaaykens@gmail.com>
2023-01-15 15:53:15 -0700

    created the structure and inital styling for the about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,19 @@
 import './About.scss'
+import Sidebar from '../Sidebar/Sidebar'
 
 function About() {
-    return(
-        <section></section>
+    return (
+        <section className='about'>
+            <Sidebar className='about__sidebar' />
+            <div className='about__container'>
+                <div className='about__subcontainer'>
+                    <p className='about__intro-copy'>Hi I'm</p>
+                    <h1 className='about__heading'>SEAMUS NAAYKENS</h1>
+                    <p className='about__copy'>Full Stack Web Developer</p>
+                    <img />
+                </div>
+            </div>
+        </section>
     )
 }
 export default About
