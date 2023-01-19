17bb10d0fb5e84023d118d747cb44ccacbc00a26
Seamus Naaykens <snaaykens@gmail.com>
2023-01-19 15:07:00 -0700

    installed emailjs library and set up logic to send email from the contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,8 +2,30 @@ import './Contact.scss'
 import linkedin from '../../assets/icons/linkedin-orange.png'
 import github from '../../assets/icons/github-orange.png'
 import instagram from '../../assets/icons/instagram-orange.png'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
 
 function Contact() {
+
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault(); // prevents the page from reloading when you hit “Send”
+
+        emailjs.sendForm('service_7q6tkba', 'template_7i1zjmc', form.current, 'seaV_v1WnYf8qtHhT')
+            .then((result) => {
+                // show the user a success message
+                alert('Email sent successfully. Thank you!')
+            }, (error) => {
+                // show the user an error
+                alert('Email could not be sent, please try again.')
+            });
+
+        e.target.reset();
+    };
+
+
     return (
         <section className='contact'>
             <div className='contact__content'>
@@ -32,19 +54,19 @@ function Contact() {
                     </a>
                 </div>
             </div>
-            <form className='contact__form'>
+            <form className='contact__form' ref={form} onSubmit={sendEmail}>
                 <h3 className='contact__heading'>SEND ME AN EMAIL</h3>
                 <div className='contact__form-input-container'>
-                    <h4 className='contact__form-heading'>NAME</h4>
-                    <input className='contact__form-input'></input>
+                    <h4 className='contact__form-heading'  >NAME</h4>
+                    <input className='contact__form-input' id='from_name' name='from_name'></input>
                 </div>
                 <div className='contact__form-input-container'>
-                    <h4 className='contact__form-heading'>EMAIL</h4>
-                    <input className='contact__form-input'></input>
+                    <h4 className='contact__form-heading' >EMAIL</h4>
+                    <input className='contact__form-input' id='reply_to' name='reply_to'></input>
                 </div>
                 <div className='contact__form-input-container'>
-                    <h4 className='contact__form-heading'>MESSAGE</h4>
-                    <input className='contact__form-input--message'></input>
+                    <h4 className='contact__form-heading' >MESSAGE</h4>
+                    <textarea className='contact__form-input--message' id='message' name='message'></textarea>
                 </div>
                 <button className='email-button'>
                     <span className="circle" aria-hidden="true">
